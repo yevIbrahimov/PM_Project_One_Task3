b6b491b3e11b04d4a8a3186967798c5d698b94bb
Yevhenii Ibrahimov <61506686+yevIbrahimov@users.noreply.github.com>
2021-01-20 11:53:54 +0200

    Update Welcome.cs

--- PollAPP/Welcome.cs
+++ PollAPP/Welcome.cs
@@ -12,6 +12,7 @@ namespace PollAPP
 			Console.WriteLine("\tHere you can be Poll Manager - it is possible to add poll, manage poll or look through statistics ");
 			Console.WriteLine("\tWhile managing poll you can show poll, delete poll, add or delete questions in chosen poll");
 			Console.WriteLine("\tAlso you can vote in choosen poll. Created by Y. Ibrahimov");
+			Console.WriteLine("\tType "Done" after entering question and answer");
 			Console.WriteLine("\tYou can exit whenever you want, just type 'exit'\n");
 		}
 	}
